2075c47d9639e8d6437a9881e11291a08037cf18
Shubhampar <119346050+Shubhampar@users.noreply.github.com>
2023-07-23 19:05:39 +0530

    push-day-5

--- Frontend/briks/src/components/HouseList.jsx
+++ Frontend/briks/src/components/HouseList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Flex, Select, Input, SimpleGrid } from '@chakra-ui/react';
+import { Box, Flex, Select, Input, SimpleGrid, Button } from '@chakra-ui/react';
 import HouseCard from './Housecard';
 import axios from 'axios';
 
@@ -15,6 +15,16 @@ const HousesListing = () => {
   const [bedroomFilter, setBedroomFilter] = useState('');
   const [priceFilter, setPriceFilter] = useState('');
   const [loading, setLoading] = useState(true);
+  const[page, setPage]=useState(1)
+
+  const priceRangeOptions = [
+    { label: 'Any', value: '' },
+    { label: 'Less than $100,000', value: '0-100000' },
+    { label: '$100,000 - $200,000', value: '100000-200000' },
+    { label: '$200,000 - $300,000', value: '200000-300000' },
+    { label: '$300,000 - $400,000', value: '300000-400000' },
+    { label: 'More than $400,000', value: '400000-' },
+  ];
 
   useEffect(() => {
     const fetchHouses = async () => {
@@ -30,7 +40,7 @@ const HousesListing = () => {
           },
         };
         // const response = await axios.get('https://real-gray-salamander-tie.cyclic.app/data/');
-        const response = await axios.get('https://enormous-library-3081-backend.onrender.com/posts/',data1);
+        const response = await axios.get(`https://enormous-library-3081-backend.onrender.com/posts/?limit=6&page=${page}`,data1);
         console.log(response.data)
         setHouses(response.data);
         setFilteredHouses(response.data);
@@ -46,7 +56,8 @@ const HousesListing = () => {
 
   useEffect(() => {
     handleFilter();
-  }, [typeFilter, bedroomFilter, priceFilter]);
+  }, [typeFilter, bedroomFilter, priceFilter,page]);
+  console.log(filteredHouses)
 
   const handleFilter = () => {
     let filteredData = houses;
@@ -56,65 +67,32 @@ const HousesListing = () => {
     }
 
     if (bedroomFilter) {
-      filteredData = filteredData.filter((house) => house.bedrooms === bedroomFilter.toString());
+      filteredData = filteredData.filter((house) => house.bedrooms === bedroomFilter);
+      console.log(filteredData)
+    }
+    if (priceFilter) {
+      const [minPrice, maxPrice] = priceFilter.split('-');
+
+      if (minPrice) {
+        filteredData = filteredData.filter((house) => house.price >= Number(minPrice));
+      }
+
+      if (maxPrice) {
+        filteredData =filteredData.filter((house) => house.price <= Number(maxPrice));
+      }
     }
 
-    if (priceFilter) {
-      filteredData = filteredData.filter((house) => house.price <= Number(priceFilter));
-    }
 
     setFilteredHouses(filteredData);
   };
+
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
-    // <Box p={4} w="80%" margin={"auto"}>
-    //   <Flex justify="space-between" align="center" mb={4}>
-    //     <Box>
-    //       <label htmlFor="type-filter">Type:</label>
-    //       <Select
-    //         id="type-filter"
-    //         value={typeFilter}
-    //         onChange={(e) => setTypeFilter(e.target.value)}
-    //       >
-    //         <option value="">All</option>
-    //         <option value="Buy">Buy</option>
-    //         <option value="Rent">Rent</option>
-    //         <option value="Sold">Sold</option>
-    //       </Select>
-    //     </Box>
-    //     <Box>
-    //       <label htmlFor="bedroom-filter">Bedroom:</label>
-    //       <Input
-    //         id="bedroom-filter"
-    //         type="number"
-    //         value={bedroomFilter}
-    //         onChange={(e) => setBedroomFilter(e.target.value)}
-    //       />
-    //     </Box>
-    //     <Box>
-    //       <label htmlFor="price-filter">Max Price:</label>
-    //       <Input
-    //         id="price-filter"
-    //         type="number"
-    //         value={priceFilter}
-    //         onChange={(e) => setPriceFilter(e.target.value)}
-    //       />
-    //     </Box>
-    //   </Flex>
-    //   <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={4}>
-    //     {filteredHouses.length > 0 ? (
-    //       filteredHouses.map((house) => (
-    //         <HouseCard key={house.id} house={house} />
-    //       ))
-    //     ) : (
-    //       <div>No houses found.</div>
-    //     )}
-    //   </SimpleGrid>
-    // </Box>
+ 
     <Box p={4} maxW="1200px" m="auto">
       <Flex flexWrap="wrap" justify="space-between" align="center" mb={4}>
         <Box w={{ sm: '100%', md: 'auto' }} >
@@ -145,19 +123,28 @@ const HousesListing = () => {
           </Select>
         </Box>
         <Box w={{ sm: '100%', md: 'auto' }} mt={{ sm: 4, md: 0 }}>
-          <label htmlFor="price-filter" style={labelStyle}>Max Price:</label>
+          {/* <label htmlFor="price-filter" style={labelStyle}>Max Price:</label>
           <Input
             id="price-filter"
             type="number"
             value={priceFilter}
             onChange={(e) => setPriceFilter(e.target.value)}
-          />
+          /> */}
+           <label htmlFor="price-filter" style={labelStyle}>Price Range:</label>
+          <Select
+            id="price-filter"
+            value={priceFilter}
+            onChange={(e) => setPriceFilter(e.target.value)}
+          >
+            {priceRangeOptions.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </Select>
         </Box>
       </Flex>
-
-      {/* Use SimpleGrid to display HouseCards */}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={4}>
-        {/* Render house cards */}
         {filteredHouses.length > 0 ? (
           filteredHouses.map((house) => (
             <HouseCard key={house.id} house={house} />
@@ -166,6 +153,18 @@ const HousesListing = () => {
           <div>No houses found.</div>
         )}
       </SimpleGrid>
+
+      <Flex justify="space-between" align="center">
+        <Button onClick={()=>setPage((prev)=>prev-1)} disabled={page === 1}>
+          Prev
+        </Button>
+        <Button  disabled={true}>
+        {page}  
+        </Button>
+        <Button onClick={()=>setPage((prev)=>prev+1)} disabled={Math.ceil(houses.length/6)===page}>
+          Next
+        </Button>
+      </Flex>
     </Box>
   );
 };
