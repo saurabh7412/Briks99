260f4196d985068b5621c10720049c6dd333cb06
saurabh7412 <121215502+saurabh7412@users.noreply.github.com>
2023-07-24 22:47:01 +0530

    9th commit

--- Frontend/briks/src/Pages/SetCarousel.jsx
+++
@@ -1,9 +0,0 @@
-function SetCarousel({ image}) {
-    return (
-      <div className="card">
-        <img className="cardimg" src={image} alt="saitama" />
-      </div>
-    );
-  }
-  
-  export { SetCarousel };
